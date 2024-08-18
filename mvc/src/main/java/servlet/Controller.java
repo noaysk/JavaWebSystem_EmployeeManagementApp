6a9411d4b51a07a74502eb3e5dfe1641103a6919
noaysk <nyasukagawa@myseneca.ca>
2024-08-18 22:09:17 +0900

    Added doGet and doPost

--- mvc/src/main/java/servlet/Controller.java
+++ mvc/src/main/java/servlet/Controller.java
@@ -1,11 +1,17 @@
 package servlet;
 
 import java.io.IOException;
+import java.sql.SQLException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import service.IdSearch;
 
 /**
  * Servlet implementation class Controller
@@ -17,17 +23,55 @@ public class Controller extends HttpServlet {
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		request.setCharacterEncoding("utf-8");
+		request.setAttribute("message", "検索条件を入力して下さい");
+
+		ServletContext context = getServletContext();
+		RequestDispatcher rd = context.getRequestDispatcher("/disp.jsp");
+		rd.forward(request, response);
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		request.setCharacterEncoding("utf-8");
+		String searchBtn = request.getParameter("btn");
+		String jsp;
+		try {
+			if (searchBtn != null && searchBtn.equals("IdSearch")) {
+				IdSearch ids = new IdSearch();
+				ids.execute(request);
+				jsp = "/disp.jsp";
+			}else {
+				request.setAttribute("errorMessage", "不正アクセスです");
+				request.setAttribute("backAddress", "controller");
+				jsp = "/error.jsp";
+			}
+
+		} catch (NumberFormatException e) {
+			e.printStackTrace();
+			request.setAttribute("errorMessage", "数値を入力して下さい");
+			request.setAttribute("backAddress", "controller");
+			jsp = "/error.jsp";
+		} catch (SQLException e) {
+			e.printStackTrace();
+			request.setAttribute("errorMessage", "JDBCのエラーです");
+			request.setAttribute("backAddress", "controller");
+			jsp = "/error.jsp";
+		} catch (Exception e) {
+			e.printStackTrace();
+			request.setAttribute("errorMessage", "エラーが発生しました");
+			request.setAttribute("backAddress", "controller");
+			jsp = "/error.jsp";
+		}
+
+		ServletContext context = getServletContext();
+		RequestDispatcher rd = context.getRequestDispatcher(jsp);
+		rd.forward(request, response);
 	}
 
 }
