0791e4b4aec0207ce835cc792c214a7c57581abd
noaysk <nyasukagawa@myseneca.ca>
2024-08-18 22:10:07 +0900

    added gettter setter

--- mvc/src/main/java/model/EmpBean.java
+++ mvc/src/main/java/model/EmpBean.java
@@ -1,5 +1,44 @@
 package model;
 
-public class EmpBean {
+import java.io.Serializable;
+
+public class EmpBean implements Serializable {
+	private int id;
+	private String name;
+	private String address;
+	private int age;
+	private String mail;
+
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getMail() {
+		return mail;
+	}
+	public void setMail(String mail) {
+		this.mail = mail;
+	}
+
 
 }
